10d96d6a6dad65b474dd8f6b42b74286dc0ca637
Ivo Araujo <ivo.souza-23@outlook.com>
2024-12-02 11:59:47 -0300

    Iniciando Projeto Backend DecoraVersiculo

---
+++ src/main/java/com/versicule/decoraversiculo/DecoraversiculoApplication.java
@@ -0,0 +1,13 @@
+package com.versicule.decoraversiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecoraversiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecoraversiculoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/versicule/decoraversiculo/DecoraversiculoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.versicule.decoraversiculo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecoraversiculoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
